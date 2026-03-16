b8a62c1088bd1b09d464cff2404163eb332c494c
Александр Абрамов <aabramov06@yandex.ru>
2026-03-16 19:35:48 +0500

    Удалил локоторы.

--- locators/feed_page_locators.py
+++ locators/feed_page_locators.py
@@ -2,18 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class FeedPageLocators:
-    # Раздел заказов
-    section_orders_list = (By.XPATH, '//ul[contains(@class, "OrderFeed_list")]')
-
+    
     # Заголовок ленты заказов
     title_of_orders_feed = (By.XPATH, '//div[contains(@class, "OrderFeed_orderFeed")]/h1')
 
     # Карточка заказа в ленте
     order_in_feed = (By.XPATH, '//li[contains(@class, "OrderHistory_listItem")][1]')
-
-    # Всплывающее окно с деталями заказа
-    modal_order = (By.XPATH, '//section[contains(@class, "Modal_modal_opened")]//div[contains'
-                             '(@class, "Modal_orderBox")]')
 
     # Заголовок всплывающего окна с деталями заказа
     title_of_modal_order = (By.XPATH, '//section[contains(@class, "Modal_modal_opened")]//div[contains(@class, '
@@ -24,9 +18,6 @@ class FeedPageLocators:
 
     # Счетчик заказов "Выполнено за сегодня"
     daily_quantity_of_orders = (By.XPATH, '//p[text()="Выполнено за сегодня:"]/following-sibling::p')
-
-    # Заказ в разделе "В работе"
-    order_in_progress = (By.XPATH, '//ul[contains(@class, "OrderFeed_orderListReady")]/li')
 
     # Номер заказа в разделе "В работе"
     number_of_order_in_progress = (By.XPATH, '//ul[contains(@class, '
